beb62643405d397d365bf18e91afe1040ed846ce
Utsav Chouhan <144726026+utsav0092@users.noreply.github.com>
2024-03-11 23:10:56 +0530

    master

--- script.js
+++ script.js
@@ -7,16 +7,33 @@ const countryEl = document.getElementById('country');
 const weaterForcastEl = document.getElementById('weather-forcast'); 
 const currentTempEl = document.getElementById('current-temp');
 
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thurday', 'Friday', 'Saturday'];
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+const API_KEY = 'bbeb9d9bc7328400ced78cb633a3c4cd';
+
 setInterval(() => {
     const time = new Date();
-    const months = time.getMonth();
+    const month = time.getMonth();
     const date = time.getDate();
     const day = time.getDay();
     const hour = time.getHours();
     const hoursIn12Hrformat = hour >= 13 ? hour%12 : hour;
     const minutes = time.getMinutes();
-    const ampm = hour >= 12 ? 'PM' : 'AM';
+    const ampm = hour >= 12 ? 'PM' : 'AM'
 
-    time.innerHTML = hoursIn12Hrformat + ':' + minutes + ' ' + `<span id="am-pm">${ampm}</span>`;
+    timeEl.innerHTML = hoursIn12Hrformat + ':' + minutes + ' ' + `<span id="am-pm">${ampm}</span>`
+    dateEl.innerHTML = days[day] + ', ' + date + ' ' + months[month];
 
 }, 1000);
+
+getWeatherDate()
+function getWeatherDate () {
+    navigator.geolocation.getCurrentPosition((success) => {
+        
+        let {latitude, longitude} = success.coords;
+        fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&appid=${API_KEY}`).then(res => res.json()).then(data => {
+            console.log(data);
+    })
+  })
+}
